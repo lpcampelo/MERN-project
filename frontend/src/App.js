0db64791e4f11890e40abafa8c0023f1eb854f3a
lpcampelo <lp.campelo@outlook.com>
2022-02-09 21:45:37 -0300

    Criados primeiros componentes

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import NewPlace from "./places/pages/NewPlaces";
+import Users from "./user/pages/Users";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>      
+    </Router>
+  );
+};
+
+export default App;
