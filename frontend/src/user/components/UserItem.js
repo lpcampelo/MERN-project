82a342a267446e99b78755895230bf1a8a6c0b00
lpcampelo <lp.campelo@outlook.com>
2022-02-09 22:52:52 -0300

    Incluído MainHeader

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Avatar from "../../shared/UIElements/Avatar";
-import Card from "../../shared/UIElements/Card";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 import "./UserItem.css";
 
 const UserItem = (props) => {
--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -4,7 +4,7 @@ import UsersList from "../components/UsersList";
 const Users = () => {
     const USERS = [
         {
-            id: 1,
+            id: 'u1',
             name: 'Leonardo Campelo', 
             image: 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvdG9ncmFwaGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80', 
             places: 3
